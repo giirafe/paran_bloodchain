28eed945be4f64d5d8fa678fbde6428929740bb5
Juyeori <dlwndus0728@ajou.ac.kr>
2022-09-07 18:05:09 +0900

    node_modules

---
+++ web-prototype-pr-master/src/main.js
@@ -0,0 +1,45 @@
+import { BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import Modal from './components/modal';
+import './mainBTN.css';
+import Login from './login';
+
+function Main() {
+  const [signup, setSignup] = useState(false);
+  const [choice, setChoice] = useState('');
+
+  const onClickChoice = (choice) => {
+    console.log(choice)
+  }
+
+  return (
+    <body>
+      <h1 className="mainName">P:LOW</h1>
+      <img className="mainLogo" src="img/logo.png" alt="로딩 실패" width="150" height="100"/>
+      <div className="slogan">
+        <h2>헌혈자 수 누계</h2>
+        <h2 className="num">8,232</h2>
+        <h2>헌혈로 세상의 가치를 잇다.</h2>
+      </div>
+      <div className='btn-box'>
+          <button className="main-btn" onClick={() => setSignup(!signup)}>사용자</button>
+          {signup && (
+        <Modal closeModal={() => setSignup(!signup)}>
+          <Login/>
+        </Modal>)}
+          <button className="main-btn" onClick={() => setSignup(!signup)}>조회 기관</button>
+          {signup && (
+        <Modal closeModal={() => setSignup(!signup)}>
+          <Login/>
+        </Modal>)}
+          <button className="main-btn" onClick={() => setSignup(!signup)}>헌혈 기관</button>
+          {signup && (
+        <Modal closeModal={() => setSignup(!signup)}>
+          <Login/>
+        </Modal>)}
+      </div>
+    </body>
+  );
+}
+
+export default Main;
