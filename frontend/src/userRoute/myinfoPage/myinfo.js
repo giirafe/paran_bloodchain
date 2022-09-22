07c35230dd88711a8824f660d2896a2785c758e6
KimUnderTheSky <96776691+KimUnderTheSky@users.noreply.github.com>
2022-09-23 01:13:27 +0900

    balanceCall성공

--- frontend/src/userRoute/myinfoPage/myinfo.js
+++ frontend/src/userRoute/myinfoPage/myinfo.js
@@ -6,6 +6,8 @@ import './myinfo.css';
 import MaterialTable from '../MaterialTable';
 // 잠시
 import BloodContract from '../../components/BloodContract';
+import { wallet_session } from '../../krcRoute/krcHome';
+
 function myinfo() {
     return(
         <body>
@@ -27,8 +29,21 @@ function myinfo() {
 }
 
 export const handleTouch = () => {
+    const send_address = wallet_session();
     console.log("hi");
     const contract = BloodContract.options
     console.log(contract);
+    const balacnes = BloodContract.call('balances', '0x028642a33362e44cd89bda306794dbee56d179bc'); //됐다
+    console.log(balacnes);
+    BloodContract.send({from: '0xd735e6b264277503066f8afb1785d6661049b831', gas: '200000000'}, "createCertificate",
+        '1234',
+        '1234',
+        '1234',
+        '1234',
+        '1234',
+        '1234',
+        '1234',
+    )
+    console.log("good");
 }
 export default myinfo;
