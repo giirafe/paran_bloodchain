e3590ea0f3b5530a5e547ebca5d9678af7f439c9
KimUnderTheSky <96776691+KimUnderTheSky@users.noreply.github.com>
2022-10-03 16:33:40 +0900

    정리
    이쁘게 정리

--- frontend/src/krcRoute/krcHome.js
+++ frontend/src/krcRoute/krcHome.js
@@ -4,20 +4,6 @@ import Button from '../components/Button'
 import BloodContract from '../components/BloodContract'
 import caver from '../klaytn/caver';
 
-//const walletFromSession = sessionStorage.getItem('walletInstance')
-//caver.klay.accounts.wallet.add(JSON.parse(walletFromSession))
-/*
-if (walletFromSession) {
-    try {
-      caver.klay.accounts.wallet.add(JSON.parse(walletFromSession))
-    } catch (e) { // error 발생시
-      // If value in sessionStorage is invalid wallet instance,
-      // remove it from sessionStorage.
-      sessionStorage.removeItem('walletInstance')
-    }
-}
-*/
-const walletInstance = caver.klay.accounts.wallet && caver.klay.accounts.wallet[0]
 
 class NFTminting extends Component {
     state = {
@@ -35,60 +21,19 @@ class NFTminting extends Component {
         this.setState({
           [e.target.name]: e.target.value,
         })
-        console.log("type is :", typeof(e.target.value));
-        console.log(caver.klay.accounts.wallet[0]);
-        console.log(caver.klay.accounts.wallet);
-        const walletInstance_1 = caver.klay.accounts.wallet && caver.klay.accounts.wallet[0]
-        console.log(walletInstance_1);
+        //console.log(caver.klay.accounts.wallet[0]);
+        //console.log(caver.klay.accounts.wallet);
+        //const walletInstance_1 = caver.klay.accounts.wallet && caver.klay.accounts.wallet[0]
+        //console.log(walletInstance_1);
     }
     
     handleSubmit = async (e) => {
-        e.preventDefault()
+      e.preventDefault()
         
-        //mintCertificate(name, id, bloodType, home_address, certificateNum,donateType,date,wallet_address)
-        const { name, id, bloodType, home_address, certificateNum,donateType,date, wallet_address} = this.state
-
-      const walletInstance = caver.klay.accounts.wallet && caver.klay.accounts.wallet[0]
-      if (!walletInstance){
-          console.error("Wallet Instance Fetch Failed");
-      } else {
-          console.log("Valid Caver Instance")
-      }
-
-      const ret1 = await BloodContract.methods.createCertificate(
-          name,
-          id,
-          bloodType,
-          home_address,
-          certificateNum,
-          donateType,
-          date
-      ).send({
-            from: walletInstance.address,// 보내는 사람 주소
-            gas: '200000000',
-          })
-      console.log("ret1 is ", ret1);
-    
-      const ret2 = await BloodContract.methods.mintCert(wallet_address, certificateNum).send({
-        from: walletInstance.address,// 보내는 사람 주소
-        gas: '200000000',
-      })
-      console.log("return is ", ret2);
-      
-      const CertLength = await BloodContract.methods.user_CertLength(wallet_address).call()
-      console.log("CertLength is ", CertLength);
-      
-      const balances = BloodContract.methods.balances(wallet_address).call()
-      console.log("balances is : ",balances);
-      const only_use_balances = BloodContract.methods.only_use_balances(wallet_address).call()
-      console.log("only_use_balances is : ",only_use_balances);
-    
-      console.log("cycle done");
-    }
-    
-    handleMinting = async () => {
+      //mintCertificate(name, id, bloodType, home_address, certificateNum,donateType,date,wallet_address)
       const { name, id, bloodType, home_address, certificateNum,donateType,date, wallet_address} = this.state
 
+      const walletInstance = caver.klay.accounts.wallet && caver.klay.accounts.wallet[0]
       if (!walletInstance){
           console.error("Wallet Instance Fetch Failed");
       } else {
@@ -216,7 +161,6 @@ class NFTminting extends Component {
               type="submit"
               title="헌혈증명서 업로드"
             />
-            <button name="헌혈증명서 발급" onClick={this.handleMinting}>헌혈증명서 발급</button>
           </form>
         )
       }
