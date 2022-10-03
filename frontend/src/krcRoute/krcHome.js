16bc9e978a83de5572aed5b2859bd14a20fa1cfd
KimUnderTheSky <96776691+KimUnderTheSky@users.noreply.github.com>
2022-10-03 09:46:23 +0900

    handleSubmit 민팅 연결중

--- frontend/src/krcRoute/krcHome.js
+++ frontend/src/krcRoute/krcHome.js
@@ -37,13 +37,50 @@ class NFTminting extends Component {
         console.log(this.props);
         
     }
-    /*
-    handleSubmit = (e) => {
+    
+    handleSubmit = async (e) => {
         e.preventDefault()
-        const { name, id, bloodType, home_address, certificateNum,donateType,date,wallet_address } = this.state
+        
         //mintCertificate(name, id, bloodType, home_address, certificateNum,donateType,date,wallet_address)
+        const { name, id, bloodType, home_address, certificateNum,donateType,date, wallet_address} = this.state
+
+      if (!walletInstance){
+          console.error("Wallet Instance Fetch Failed");
+      } else {
+          console.log("Valid Caver Instance")
+      }
+
+      const ret1 = await BloodContract.methods.createCertificate(
+          name,
+          id,
+          bloodType,
+          home_address,
+          certificateNum,
+          donateType,
+          date
+      ).send({
+            from: walletInstance.address,// 보내는 사람 주소
+            gas: '200000000',
+          })
+      console.log("ret1 is ", ret1);
+    
+      const ret2 = await BloodContract.methods.mintCert(wallet_address, certificateNum).send({
+        from: walletInstance.address,// 보내는 사람 주소
+        gas: '200000000',
+      })
+      console.log("return is ", ret2);
+      
+      const CertLength = await BloodContract.methods.user_CertLength(wallet_address).call()
+      console.log("CertLength is ", CertLength);
+      
+      const balances = BloodContract.methods.balances(wallet_address).call()
+      console.log("balances is : ",balances);
+      const only_use_balances = BloodContract.methods.only_use_balances(wallet_address).call()
+      console.log("only_use_balances is : ",only_use_balances);
+    
+      console.log("cycle done");
     }
-    */
+    
     handleMinting = async () => {
       const { name, id, bloodType, home_address, certificateNum,donateType,date, wallet_address} = this.state
 
