4bfc20af56f74dc1e7c375c46be95ec730b1deb5
KimUnderTheSky <96776691+KimUnderTheSky@users.noreply.github.com>
2022-10-06 14:57:33 +0900

    작업중

--- frontend/src/userRoute/userHome.js
+++ frontend/src/userRoute/userHome.js
@@ -11,13 +11,26 @@ function Home() {
     const submainHandler = () => {
         navigate('/providekey')
     }
+
     const walletInstance = caver.klay.accounts.wallet && caver.klay.accounts.wallet[0]
     // address를 통해서 블록체인 내에 매핑에 접근하면 됨
-    const name = "";
-    const id = "";
-    const donateType = "";
-    const date = "";
+    //const cert = BloodContract.methods.InquiryTo(walletInstance.address,1234,0).call()
+    /*
+    const get_data = () => cert.then(function(result) {
+        const data = result; // promise의 result object값이 data로 들어감
+        //console.log(data.get_name); 
+        const name = data.get_name;
+        const id = data.get_id;
+        const donateType = data.get_donateType;
+        const date = data.get_date;
+        //let obj = {name: name, id: id, donateType: donateType, date: date};
+        let arr = {name, id, donateType, date};
+        return arr
+    });
 
+    const cert_data =  await get_data();
+    console.log(cert_data);
+    */
     return(
         <section>
             <Header/>
