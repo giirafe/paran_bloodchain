b3a04bafcafbe47037aabad8e545492cafd34668
KimUnderTheSky <96776691+KimUnderTheSky@users.noreply.github.com>
2022-10-07 15:20:17 +0900

    조회기능성공

--- frontend/src/userRoute/userHome.js
+++ frontend/src/userRoute/userHome.js
@@ -1,5 +1,5 @@
 import '../App.css';
-import React, {useState, useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/layout/header';
 import {useNavigate} from 'react-router-dom';
 import './userHome.css';
@@ -11,78 +11,20 @@ function Home() {
     const submainHandler = () => {
         navigate('/providekey')
     }
-    state = {
-        name: "",
-    }
+    var [number, setNumber] = useState("");
     const walletInstance = caver.klay.accounts.wallet && caver.klay.accounts.wallet[0]
-    const cert = BloodContract.methods.InquiryTo(walletInstance.address,1234,0).call()
-    console.log("Cert is ", cert);
-    getCertdata = async () => {
+    //const cert = BloodContract.methods.InquiryTo(walletInstance.address,1234,0).call()
+    //console.log("Cert is ", cert);
+    const getCertdata = async () => {
+        const cert = BloodContract.methods.get(walletInstance.address,,0).call()
+        console.log("cert is :",cert);
+        const cert_data = await cert;
+        console.log("cert data is :",cert_data);
+        setNumber(number = cert_data.get_name);
+    }
+    getCertdata();
+    console.log("state: ", number);
     
-    }
-    /*
-    getCertdata = async () => {
-        const cert = await BloodContract.methods.InquiryTo(walletInstance.address,1234,0).call()
-        console.log()
-        //cert.then((response) => {console.log(response)});
-        return {
-            get_name: cert.then((response) => {
-                console.log(response);
-                console.log("test");
-                
-            }),
-        }
-    }
-    */
-    //const name = data();
-    //console.log("name is ", name);
-    /*
-    const get_cert = async () => {
-        await BloodContract.methods.user_CertLength(walletInstance.address).call().then((totalCertLength) => {
-            if(!totalCertLength) return []
-            const feed = []
-            for (let i = 0; i < totalCertLength - 1; i++) {
-                const cert = BloodContract.methods.InquiryTo(walletInstance.address, 1234, i).call()
-                feed.push(cert)
-            }
-            console.log("되나?", feed);
-            return Promise.all(feed)
-        })
-    }
-    
-    const data = get_cert();
-    console.log("제발돼라: ", data);
-    */
-
-    //cert.then((response) => console.log("response:",response.get_name))
-
-    /*
-    console.log("type of cert is:", cert);
-    cert.then(function(response){
-        console.log('response', response);
-    }).catch(function(reason){
-        console.log('reason', reason);
-    })
-    */
-    /*
-    const get_data = () => cert.then(function(result) {
-        const data = result; // promise의 result object값이 data로 들어감
-        //console.log(data.get_name); 
-        const name = data.get_name;
-        const id = data.get_id;
-        const donateType = data.get_donateType;
-        const date = data.get_date;
-        let obj = {name: name, id: id, donateType: donateType, date: date};
-        console.log(obj);
-        return obj
-    });
-    async function a(){
-        const cert_data = await get_data();
-        return cert_data
-    }
-    const b = a();
-    console.log("b is :", b);
-    */
     return(
         <section>
             <Header/>
