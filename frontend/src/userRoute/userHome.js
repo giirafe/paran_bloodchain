dad3e7dccec876aff032f61a878902f1fb96d483
JuneSeo <seojune408@gmail.com>
2022-10-11 20:22:45 +0900

    getting User Data from BloodContract Blockchain Network on userHome.js

--- frontend/src/userRoute/userHome.js
+++ frontend/src/userRoute/userHome.js
@@ -1,8 +1,10 @@
 import '../App.css';
-import React, {useState, useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/layout/header';
 import {useNavigate} from 'react-router-dom';
 import './userHome.css';
+import BloodContract from '../components/BloodContract';
+import caver from '../klaytn/caver';
 
 function Home() {
     const navigate = useNavigate();
@@ -11,7 +13,48 @@ function Home() {
         navigate('/providekey')
     }
 
-    const [count, setCount] = useState(0);
+    var [name, setName] = useState("");
+    var [id, setId] = useState("");
+    var [date, setDate] = useState("");
+    var [donateType, setType] = useState("");
+    const walletInstance = caver.klay.accounts.wallet && caver.klay.accounts.wallet[0]
+
+    const wallet_session = () => {
+        console.log("Wallet Session Loading");
+        const data = JSON.parse(sessionStorage.getItem("walletInstance"));
+        console.log(data.address);
+        return data.address // 세션 스토리지 address값반환
+    }
+      
+
+    const getCertdata = async () => {
+        // console.log("Wallet Instance Address : ",walletInstance.address);
+        const send_address = wallet_session();
+        try{
+            const cert = BloodContract.methods.InquiryTo(send_address,1234,0).call()
+            const cert_data = await cert;
+            console.log("cert data is :",cert_data);
+            setName(name = cert_data.get_name);
+            setId(id = cert_data.get_id);
+            setDate(date = cert_data.get_date);
+            setType(donateType = cert_data.get_donateType);
+        }catch(err){
+            console.log(err);
+        }
+        console.log("cycle done");
+    }
+    const check_existence = async()=>{
+        const inquiry_pw = await BloodContract.methods.Check_Existence3(10).call();
+        console.log("Checking Existence Thorugh Blood Contract : ",inquiry_pw);
+    }
+    
+    console.log("Checking Existence Through Smart Contract");
+    check_existence();
+
+    console.log("Getting Certificate Data");
+    getCertdata();
+    
+    console.log("state: ", name);
 
     return(
         <section>
