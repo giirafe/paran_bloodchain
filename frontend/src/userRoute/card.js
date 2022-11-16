c77cd4e12c979b06417d7a62cc9a278b096b8a55
Juyeori <dlwndus0728@ajou.ac.kr>
2022-11-16 20:54:14 +0900

    userHome

---
+++ frontend/src/userRoute/card.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+//import {TiChevronLeftOutline, TiChevronRightOutline} from 'https://cdn.skypack.dev/react-icons/ti';
+import './card.scss';
+import {Icon} from '@iconify/react';
+
+const CARDS = 3;
+const MAX_VISIBILITY = 3;
+
+const Card = ({title, content}) => (
+    <div className='card'>
+        <div className="front">
+            앞
+        </div>
+        <div className="back">
+            <h2>{title}</h2>
+            <p>{content}</p>
+        </div>
+    </div>
+);
+
+const Carousel = ({children}) => {
+  const [active, setActive] = useState(0);
+  const count = React.Children.count(children);
+  
+  return (
+    <div className='carousel'>
+      {active > 0 && <button className='nav left' onClick={() => setActive(i => i - 1)}><Icon icon="typcn:chevron-left-outline" /></button>}
+      {React.Children.map(children, (child, i) => (
+        <div className='card-container' style={{
+            '--active': i === active ? 1 : 0,
+            '--offset': (active - i) / 3,
+            '--direction': Math.sign(active - i),
+            '--abs-offset': Math.abs(active - i) / 3,
+            'pointer-events': active === i ? 'auto' : 'none',
+            'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+            'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
+          }}>
+          {child}
+        </div>
+      ))}
+      {active < count - 1 && <button className='nav right' onClick={() => setActive(i => i + 1)}><Icon icon="typcn:chevron-right-outline"/></button>}
+    </div>
+  );
+};
+
+/*
+const HomeCard = () => (
+  <div className='app'>
+    <Carousel>
+      {[...new Array(CARDS)].map((_, i) => (
+        <Card title={'Card ' + (i + 1)} content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
+      ))}
+    </Carousel>
+  </div>
+);*/
