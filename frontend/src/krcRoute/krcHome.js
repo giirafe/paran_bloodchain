2a4bf68d0c489d975f536bfb70218764a3961cb3
Juyeori <dlwndus0728@ajou.ac.kr>
2022-11-18 17:14:46 +0900

    준

--- frontend/src/krcRoute/krcHome.js
+++ frontend/src/krcRoute/krcHome.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import BloodContract from '../components/BloodContract'
@@ -23,144 +23,196 @@ if (walletFromSession) {
 */
 
 console.log("klaytn wallet is :", caver.klay.accounts.wallet)
-class NFTminting extends Component {
-    state = {
-      name: '',
-      id: '',
-      bloodType: '',
-      home_address: '',
-      certificateNum: '',
-      donateType: '',
-      date: '',
-      wallet_address:'',
-    }
-    
-    handleInputChange = (e) => {
-        this.setState({
-          [e.target.name]: e.target.value,
-        })
-    }
+function NFTminting (){
+  const [name, setName] = useState('')
+  const [id, setId] = useState('')
+  const [bloodType, setBloodType] = useState('')
+  const [homeAddress, setHomeAddress] = useState('')
+  const [certificateNum, setCertificateNum] = useState('')
+  const [donateType, setDonateType] = useState('전혈')
+  const [date, setDate] = useState('')
+  const [walletAddress, setWalletAddress] = useState('')
 
-    handleSubmit = async (e) => {
-        e.preventDefault()
-        const { name, id, bloodType, home_address, certificateNum,donateType,date,wallet_address } = this.state
-        await mintCertificate(name, id, bloodType, home_address, certificateNum,donateType,date,wallet_address)
-        await window.location.reload();
-    }
-    //test
-    handleClick = (e) => {
-      console.log("Hi");
-    }
+  const handleName = (e) => {
+      setName(e.target.value)
+  }
 
-    
-    render() {
-        const { name, id, bloodType, home_address, certificateNum,donateType,date,wallet_address } = this.state
-        return (
-          <body>
-            <MainHeader/>
-            <div className="space"></div>
-            <div className="Board_css">
-              <BootstrapTable>
-                  <TableHeaderColumn width='350' dataField='mint_transaction' dataAlign='center'>발행 기록</TableHeaderColumn>
-                  <TableHeaderColumn width='200' dataField='time_stamp' dataAlign='center'>발행일</TableHeaderColumn>
-                  <TableHeaderColumn width='200' dataField='mint_address' dataAlign='center' isKey={true}>발행 주소</TableHeaderColumn>
-              </BootstrapTable>
+  const handleId = (e) => {
+    setId(e.target.value)
+  }
+
+  const handleBloodType = (e) => {
+    setBloodType(e.target.value)
+  }
+
+  const handleHomeAddress = (e) => {
+    setHomeAddress(e.target.value)
+  }
+
+  const handleCertificateNum = (e) => {
+    setCertificateNum(e.target.value)
+  }
+
+  const handleDonateType = (e) => {
+    setDonateType(e.target.value)
+  }
+
+  const handleDate = (e) => {
+    setDate(e.target.value)
+  }
+
+  const handleWalletAddress = (e) => {
+    setWalletAddress(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+      await mintCertificate(name, id, bloodType, homeAddress, certificateNum,donateType,date,walletAddress)
+      await window.location.reload();
+  }
+
+
+      return (
+        <body>
+          <MainHeader/>
+          <div className="space"></div>
+          <div className="Board_css">
+            <BootstrapTable>
+                <TableHeaderColumn width='350' dataField='mint_transaction' dataAlign='center'>발행 기록</TableHeaderColumn>
+                <TableHeaderColumn width='200' dataField='time_stamp' dataAlign='center'>발행일</TableHeaderColumn>
+                <TableHeaderColumn width='200' dataField='mint_address' dataAlign='center' isKey={true}>발행 주소</TableHeaderColumn>
+            </BootstrapTable>
+          </div>
+          <div className="mint">
+            <form className="NFTminting" onSubmit={handleSubmit}>
+            <label>헌혈증명서 발행</label>
+            <br/>
+            <input
+              className="NFTminting_css"
+              name="name"
+              label="헌혈자 이름"
+              value={name}
+              onChange={handleName}
+              placeholder="헌혈자 이름을 입력하시오."
+              required
+            />
+            
+            <input
+              className="NFTminting_css"
+              name="id"
+              label="주민등록번호"
+              value={id}
+              onChange={handleId}
+              placeholder="헌혈자의 주민등록번호를 입력하시오."
+              required
+            />
+            
+            <input
+              className="NFTminting_css"
+              name="bloodType"
+              label="혈액형"
+              value={bloodType}
+              onChange={handleBloodType}
+              placeholder="헌혈자의 혈액형을 입력하시오."
+              required
+            />
+
+            <input
+              className="NFTminting_css"
+              name="home_address"
+              label="주소"
+              value={homeAddress}
+              onChange={handleHomeAddress}
+              placeholder="헌혈자의 주소를 입력하시오."
+              required
+            />
+
+            <input
+              className="NFTminting_css"
+              name="certificateNum"
+              label="증명서 번호"
+              value={certificateNum}
+              onChange={handleCertificateNum}
+              placeholder="증명서 번호를 입력하시오."
+              required
+            />
+
+            <div className="typeCheck">
+              <p>헌혈 종류를 선택하세요.</p>
+              <label>
+              <input 
+              name="donateType"
+              value="전혈"
+              type="radio"
+              checked={donateType === "전혈"}
+              onChange={handleDonateType}/>
+              <br/>
+              전혈
+              </label>
+
+              <label>
+              <input 
+              name="donateType"
+              value="혈장"
+              type="radio"
+              checked={donateType === "혈장"}
+              onChange={handleDonateType}/>
+              <br/>
+              혈장
+              </label>
+
+              <label>
+              <input 
+              name="donateType"
+              value="혈소판"
+              type="radio"
+              checked={donateType === "혈소판"}
+              onChange={handleDonateType}/>
+              <br/>
+              혈소판
+              </label>
+
+              <label>
+              <input 
+              name="donateType"
+              value="혈장+혈소판"
+              type="radio"
+              checked={donateType === "혈장+혈소판"}
+              onChange={handleDonateType}/>
+              <br/>
+              혈장 + 혈소판
+              </label>
             </div>
-            <div className="mint">
-              <form className="NFTminting" onSubmit={this.handleSubmit}>
-              <label>헌혈증명서 발행</label>
-              <br/>
-              <input
-                className="NFTminting_css"
-                name="name"
-                label="헌혈자 이름"
-                value={name}
-                onChange={this.handleInputChange}
-                placeholder="헌혈자 이름을 입력하시오."
-                required
-              />
-              
-              <input
-                className="NFTminting_css"
-                name="id"
-                label="주민등록번호"
-                value={id}
-                onChange={this.handleInputChange}
-                placeholder="헌혈자의 주민등록번호를 입력하시오."
-                required
-              />
-              
-              <input
-                className="NFTminting_css"
-                name="bloodType"
-                label="혈액형"
-                value={bloodType}
-                onChange={this.handleInputChange}
-                placeholder="헌혈자의 혈액형을 입력하시오."
-                required
-              />
 
-              <input
-                className="NFTminting_css"
-                name="home_address"
-                label="주소"
-                value={home_address}
-                onChange={this.handleInputChange}
-                placeholder="헌혈자의 주소를 입력하시오."
-                required
-              />
 
-              <input
-                className="NFTminting_css"
-                name="certificateNum"
-                label="증명서 번호"
-                value={certificateNum}
-                onChange={this.handleInputChange}
-                placeholder="증명서 번호를 입력하시오."
-                required
-              />
-
-              <input
-                className="NFTminting_css"
-                name="donateType"
-                label="헌혈 종류"
-                value={donateType}
-                onChange={this.handleInputChange}
-                placeholder="헌혈 종류를 입력하시오."
-                required
-              />
-
-              <input
-                className="NFTminting_css"
-                name="date"
-                label="날짜"
-                value={date}
-                onChange={this.handleInputChange}
-                placeholder="오늘 날짜를 입력하시오."
-                required
-              />
-              <br/>
-              <input
-                className="NFTminting_css"
-                name="wallet_address"
-                label="지갑 주소"
-                value={wallet_address}
-                onChange={this.handleInputChange}
-                placeholder="지갑 주소를 입력하시오."
-                required
-              />
-              <br/>
-              <Button
-                className="Certificate__upload"
-                type="submit"
-                title="헌혈증명서 업로드"
-              />
-            </form>
-            </div>
-          </body>
-          
-        )
-      }
+            <input
+              className="NFTminting_css"
+              name="date"
+              label="날짜"
+              value={date}
+              onChange={handleDate}
+              placeholder="오늘 날짜를 입력하시오."
+              required
+            />
+            <br/>
+            <input
+              className="NFTminting_css"
+              name="wallet_address"
+              label="지갑 주소"
+              value={walletAddress}
+              onChange={handleWalletAddress}
+              placeholder="지갑 주소를 입력하시오."
+              required
+            />
+            <br/>
+            <Button
+              className="Certificate__upload"
+              type="submit"
+              title="헌혈증명서 업로드"
+            />
+          </form>
+          </div>
+        </body>
+      )
 }
 
 export const wallet_session = () => {
