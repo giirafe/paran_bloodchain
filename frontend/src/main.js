db8f36da6b6504aa0ae7febb6c888aae4fda8080
Juyeori <dlwndus0728@ajou.ac.kr>
2022-12-04 15:49:01 +0900

    add vision UI first try

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import './mainBTN.css';
 import isAdmin from './components/isAdmin';
 import Auth from './components/AuthClone';
 import MainHeader from './components/layout/mainHeader';
+import {FullPage, Slide} from 'react-full-page';
 
 function Main() {
   const [signup, setSignup] = useState(false);
@@ -60,8 +61,9 @@ function Main() {
   }
 
   return (
-    
-    <div>
+    <FullPage controls controlsProps={{className:'controller'}}>
+      <Slide>
+      <div id="mainbox">
       <MainHeader/>
         <div className="img">
           <img className="mainIMG" src="img/main.png" alt="로딩 실패"/>
@@ -71,7 +73,7 @@ function Main() {
         <br/>
         <br/>
         <br/>
-        <div className="slogan">
+        <div className="slogan" id="slogan">
           <h1>헌혈로 세상의 가치를 잇다</h1>
           <h2>당신의 헌혈이 살릴 수 있어요</h2>
           <br/>
@@ -108,6 +110,44 @@ function Main() {
         </div>
 
     </div>
+      </Slide>
+            <Slide>
+                <div className="introduce" id="introduce">
+                <div className="space"></div>
+                <div className="space"></div>
+                  <h2>헌혈로 세상의 가치를 잇다.</h2>
+                  <p>P:LOW는 더 나은 세상을 위해 다음과 같은 가치를 제공해요.</p>
+                  <div className="introduceBox_1">
+                    헌혈 증명서, 헌혈 증서의 NFT화
+                  </div>
+                  <div className="introduceBox_2">
+                    헌혈 커뮤니티를 통한 헌혈 증서 기부
+                  </div>
+                  <div className="introduceBox_3">
+                    헌혈 기록 조회 과정 간소화
+                  </div>
+                </div>
+            </Slide>
+            <Slide>
+                <div className="vision" id="vision">
+                  <div className="space"></div>
+                  <div className="space"></div>
+                  <h2>P:LOW</h2>
+                  <p>블록체인 기반 전자헌혈증 플랫폼</p>
+                  <div className="visionBox_1">
+                    <h2>P(Blood)</h2>
+                    <h3>피</h3>
+                  </div>
+                  <div className="width_"></div>
+                  <div className='height_'></div>
+                  <div className="visionBox_2">
+                    <h2>FLOW</h2>
+                    <h3>흐르다</h3>
+                  </div>
+                </div>
+            </Slide>
+    </FullPage>
+
   );
 }
 
---
+++ frontend/src/vision.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {FullPage, Slide} from 'react-full-page';
+
+function Vision() {
+    
+    
+    return (
+        <FullPage controls>
+            <Slide>
+                <h1>팀 목표</h1>
+            </Slide>
+            <Slide>
+                <h2>비전</h2>
+            </Slide>
+        </FullPage>
+    )
+}
